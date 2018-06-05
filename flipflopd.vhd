0fbe01074dc7e374cb35c624beda0f61ae9d810e
Caio Torre <caiosanches@live.com>
2018-06-04 21:55:39 -0300

    Edited components
    FlipFlopD now takes in Enable and ASReset, will test ASAP;
    Added LatchSR to store log-in status

--- flipflopd.vhd
+++ flipflopd.vhd
@@ -2,15 +2,20 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity flipflopd is 
-	port (d, clock: in std_logic;
+	port (d, clock, en, r: in std_logic;
 			q       : out std_logic);
 end flipflopd;
 
 architecture memory of flipflopd is 
 begin 
-	process
+	process(clock, en)
 	begin
-		wait until clock'event and clock = '1';
+		wait until clock'event and clock = '1' and en = '1';
 		q <= d;
+	end process;
+	process(r)
+	begin 
+		if (r = '1') then q <= '0'
+		end if;
 	end process;
 end memory;
--- projeto.vhd
+++ projeto.vhd
@@ -11,37 +11,58 @@ entity projeto is
 end projeto;
 
 architecture memory of projeto is
-	signal loggedIn, keyCorrect, updateSENHA: std_logic;
-	signal bcd0, bcd1: std_logic_vector(0 to 3);
+	signal EnterTrigger, PassOK, IsLoggedIn, SystemLockdown, SetLogin, ResetLogin: std_logic;
 	signal senhaAtual: std_logic_vector(0 to 5);
+	signal bcd0, bcd1: std_logic_vector(0 to 3);
+	signal attempts: std_logic_vector(0 to 2);
+	--signal loggedIn, keyCorrect, updateSENHA: std_logic;
+	--signal bcd0, bcd1: std_logic_vector(0 to 3);
+	--signal senhaAtual: std_logic_vector(0 to 5);
 	component bcd
 		port (code: in  std_logic_vector(0 to 3);
 				leds: out std_logic_vector(0 to 6));
 	end component;
 	component flipflopd
-		port (d, clock: in std_logic;
+		port (d, clock, en, r: in std_logic;
 				q 		  : out std_logic);
 	end component;
+	component latchsr 
+		port (s, r: in std_logic;
+				q		: out std_logic);
+	end component;
 begin
-	loggedState: flipflopd port map (keyCorrect, chaveE, loggedIn);
-	senha0: flipflopd port map (entradaSENHA(0), updateSENHA, senhaAtual(0));
-	senha1: flipflopd port map (entradaSENHA(1), updateSENHA, senhaAtual(1));
-	senha2: flipflopd port map (entradaSENHA(2), updateSENHA, senhaAtual(2));
-	senha3: flipflopd port map (entradaSENHA(3), updateSENHA, senhaAtual(3));
-	senha4: flipflopd port map (entradaSENHA(4), updateSENHA, senhaAtual(4));
-	senha5: flipflopd port map (entradaSENHA(5), updateSENHA, senhaAtual(5));
-	num0: bcd port map (bcd0, numero0);
-	num1: bcd port map (bcd1, numero1);
+	PassOK <=  ((entradaSENHA(0) xnor senhaAtual(0)) and
+				(entradaSENHA(1) xnor senhaAtual(1)) and
+				(entradaSENHA(2) xnor senhaAtual(2)) and
+				(entradaSENHA(3) xnor senhaAtual(3)) and
+				(entradaSENHA(4) xnor senhaAtual(4)) and
+				(entradaSENHA(5) xnor senhaAtual(5)));
+	
+	SetLogin <= 	EnterTrigger and PassOK;
+	ResetLogin <= 	not EnterTrigger;
+	loggedState: latchsr port map (SetLogin, ResetLogin, IsLoggedIn);
+	
+	senha0: flipflopd port map (entradaSENHA(0), chaveS, IsLoggedIn, '0', senhaAtual(0));
+	senha1: flipflopd port map (entradaSENHA(1), chaveS, IsLoggedIn, '0', senhaAtual(1));
+	senha2: flipflopd port map (entradaSENHA(2), chaveS, IsLoggedIn, '0', senhaAtual(2));
+	senha3: flipflopd port map (entradaSENHA(3), chaveS, IsLoggedIn, '0', senhaAtual(3));
+	senha4: flipflopd port map (entradaSENHA(4), chaveS, IsLoggedIn, '0', senhaAtual(4));
+	senha5: flipflopd port map (entradaSENHA(5), chaveS, IsLoggedIn, '0', senhaAtual(5));
+	
 	process(chaveE)
 	begin
-		if chaveE'event and chaveE = '1' then 
-			keyCorrect <= 	(entradaSENHA(0) xnor senhaAtual(0)) and
-								(entradaSENHA(1) xnor senhaAtual(1)) and
-								(entradaSENHA(2) xnor senhaAtual(2)) and
-								(entradaSENHA(3) xnor senhaAtual(3)) and
-								(entradaSENHA(4) xnor senhaAtual(4)) and
-								(entradaSENHA(5) xnor senhaAtual(5));
+		if chaveE'event and SystemLockdown = '0' then 
+			EnterTrigger <= chaveE;
 	end process
-	process(keyCorrect)
-		
+	
+	strike1: flipflopd port map ( not PassOK, EnterTrigger, '1', IsLoggedIn, attempts(0));
+	strike2: flipflopd port map (attempts(0), EnterTrigger, '1', IsLoggedIn, attempts(1));
+	strike3: flipflopd port map (attempts(1), EnterTrigger, '1', IsLoggedIn, attempts(2));
+	SystemLockdown <= (attempts(0) nand attempts(1) nand attempts(2));
+	
+	num0: bcd port map (bcd0, numero0);
+	num1: bcd port map (bcd1, numero1);
+	
+	ledEstado <= IsLoggedIn;
+	ledErro <= not SystemLockdown;
 end memory;
