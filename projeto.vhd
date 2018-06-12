f249f432fe60462857e8307f7c9336ea75b13251
Caio Torre <caiosanches@live.com>
2018-06-12 14:46:51 -0300

    Remodeling as a Finite State Machine
    Rebuilding project from the ground up to allow the usage of the LCD;
    Will add BCD support;

--- projeto.vhd
+++ projeto.vhd
@@ -4,109 +4,99 @@ use ieee.std_logic_unsigned.all;
 
 entity projeto is 
 	port (entradaSENHA: 	in std_logic_vector(0 to 5);
+	
 			chaveE:			in std_logic;
 			chaveS: 		in std_logic;
+			
 			resetStrikes:	in std_logic;
+			
 			masterClock: in std_logic;
+			
 			ledEstado: 		out std_logic;
 			ledErro:		out std_logic;
+			
 			numero0:		out std_logic_vector(0 to 6);
 			numero1: 	out std_logic_vector(0 to 6);
+			
 			lcd_rw, lcd_rs, lcd_e  : OUT   STD_LOGIC;  --read/write, setup/data, and enable for lcd
 			lcd_dataout   : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0);
+			
 			senhaOKLED: out std_logic);
 end projeto;
 
 architecture cofre of projeto is
-	signal EnterTrigger, PassOK, IsLoggedIn, SystemLockdown, OnKeyUpdate, SetLogin, ResetLogin, NewKey, WrongPass, NewETrig, NewAttempt, ajuste, ResetLockdown: std_logic;
-	signal senhaAtual: std_logic_vector(0 to 5);
-	signal ultimaTent: std_logic_vector(0 to 5);
-	signal bcd0, bcd1: std_logic_vector(0 to 3);
-	signal attempts: std_logic_vector(0 to 3);
-	signal LCD_BUSY: std_logic;
-	signal LCD_UPDT: std_logic;
-	signal SYS_STATE: std_logic_vector(0 to 1);
-	component bcd
-		port (code: in  std_logic_vector(0 to 3);
-				leds: out std_logic_vector(0 to 6));
-	end component;
-	component flipflopd
-		port (d, clock, en, r: in std_logic;
-				q 		  : out std_logic);
-	end component;
+	TYPE CONTROL IS (waiting_input, test_input, login_success, read_new_key, out_of_attempts);
+	SIGNAL STATE: CONTROL;
+
 	component compare
 		port (inp1, inp2: in std_logic_vector(0 to 5);
 				res: out std_logic);
 	end component;
-	component memoryLine
-		port (Data: in std_logic_vector(0 to 5);
-				Clock, Enable, Reset: in std_logic;
-				Qout: out std_logic_vector(0 to 5));
-	end component;
-	component upcounter is 
-		port (
-			clock: in std_logic;
-			enable: in std_logic;
-			reset: in std_logic;
-			output: out std_logic_vector(0 to 1));
-	end component;
-	component LCDHandler is
-		port (
-			clock: in std_logic;
-			--updateScreen: in std_logic;
-			system_state: in std_logic_vector(0 to 1);
-			busy       : OUT   STD_LOGIC := '1';  --lcd controller busy/idle feedback
-			rw, rs, e  : OUT   STD_LOGIC;  --read/write, setup/data, and enable for lcd
-			lcd_dataout   : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0));
-	end component;
+
+signal PassEQ:	std_logic := '1';
+signal PassOK:	std_logic := '0';
+signal lastE:	std_logic := '1';
+signal lastK:	std_logic_vector(0 to 5) := "000000";
+signal storeK:	std_logic_vector(0 to 5) := "000000";
 begin
-	process(IsLoggedIn, SystemLockdown, EnterTrigger)
-	begin
-		if (SystemLockdown = '0') then 
-			SYS_STATE <= "11";
-		elsif (IsLoggedIn = '1') then 
-			SYS_STATE <= "10";
-		elsif (EnterTrigger = '1') then 
-			SYS_STATE <= "01";
-		else
-			SYS_STATE <= "00";
-		end if;
-	end process;
-	masterHandler: LCDHandler port map (masterClock, SYS_STATE, LCD_BUSY, lcd_rw, lcd_rs, lcd_e, lcd_dataout);
-	--LCD_UPDT <= IsLoggedIn;
-	LCD_UPDT <= '1';
-	senhaOKLED <= PassOK;
-	checkPass: compare port map (entradaSENHA, senhaAtual, PassOK);
-	
-	NewKey <= not chaveS;
-	EnterTrigger <= chaveE and SystemLockdown;
-	loggedState: flipflopd port map (SetLogin, OnKeyUpdate, '1', ResetLogin, IsLoggedIn);
-	SetLogin <= IsLoggedIn or PassOK;
-	ResetLogin <= not EnterTrigger;
-	
-	guardaSenha: memoryLine port map (entradaSENHA, NewKey, IsLoggedIn, '0', senhaAtual);
-	tentaE: flipflopd port map (EnterTrigger, OnKeyUpdate, '1', IsLoggedIn, NewETrig);
-	
-	guardaUltimaTent: memoryLine port map (entradaSENHA, OnKeyUpdate, '1', '0', ultimaTent);
-	checkForNewAttempt: compare port map (entradaSENHA, ultimaTent, NewAttempt);
-	
-	process(EnterTrigger)
-	begin 
-		OnKeyUpdate <= EnterTrigger and (NewETrig nand NewAttempt);
-	end process;
-	
-	WrongPass <= not PassOK;
-	--countLockdown: upcounter port map (OnKeyUpdate, not IsLoggedIn, ResetLockdown, attempts);
-	strike1: flipflopd port map (  WrongPass, EnterTrigger, not IsLoggedIn, ResetLockdown, attempts(0));
-	strike2: flipflopd port map (attempts(0), EnterTrigger, not IsLoggedIn, ResetLockdown, attempts(1));
-	strike3: flipflopd port map (attempts(1), EnterTrigger, not IsLoggedIn, ResetLockdown, attempts(2));
-	
-	SystemLockdown <= not (attempts(0) and attempts(1) and attempts(2));
-	ResetLockdown <= IsLoggedIn or not resetStrikes;
-		
-	num0: bcd port map (bcd0, numero0);
-	num1: bcd port map (bcd1, numero1);
-	
-	ledEstado <= IsLoggedIn;
-	ledErro <= not SystemLockdown;
+	compareLast:	compare port map(entradaSENHA, lastK, PassEQ);
+	compareKey:		compare port map(entradaSENHA, storeK, PassOK);
+	senhaOKLED <= PassEQ;
+	PROCESS(masterClock)
+		VARIABLE attempts : INTEGER := 0;
+	BEGIN
+		IF(masterClock'EVENT and masterClock = '1') THEN
+			CASE state IS 
+				WHEN waiting_input =>
+					IF (chaveE = '1' and lastE = '0') THEN 
+						state <= test_input;
+						lastE <= '1';
+					ELSIF (chaveE = '1' and PassEQ = '0') THEN 
+						state <= test_input;
+					END IF;
+					IF (chaveE = '0') THEN 
+						lastE <= '0';
+					END IF;
+					
+				WHEN test_input =>
+					IF (PassOK = '1') THEN 
+						state <= login_success;
+					ELSE
+						lastK <= entradaSENHA;
+						attempts := attempts + 1;
+						IF (attempts < 3) THEN 
+							state <= waiting_input;
+						ELSE 
+							state <= out_of_attempts;
+						END IF;
+					END IF;
+					
+				WHEN login_success =>
+					ledEstado <= '1';
+					attempts := 0;
+					IF (chaveE = '0') THEN 
+						ledEstado <= '0';
+						state <= waiting_input;
+					ELSIF (chaveS = '1') THEN 
+						state <= read_new_key;
+					END IF;
+					
+				WHEN read_new_key =>
+					IF (chaveE = '0') THEN 
+						state <= waiting_input;
+					ELSIF (chaveS = '0') THEN
+						storeK <= entradaSENHA;
+						state <= login_success;
+					END IF;
+					
+				WHEN out_of_attempts =>
+					ledErro <= '1';
+					IF (resetStrikes = '0') THEN --Pushbutton invertido
+						ledErro <= '0';
+						attempts := 0;
+						state <= waiting_input;
+					END IF;
+			END CASE;
+		END IF;
+	END PROCESS;
 end cofre;
