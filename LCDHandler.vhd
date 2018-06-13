dd84f9f83bc94050b3d3b7f71624f27ec9f6c950
Caio Torre <caiosanches@live.com>
2018-06-13 17:19:48 -0300

    Added current input to LCD (decimals)
    Occasionally the system prints garbage, will look into that

--- LCDHandler.vhd
+++ LCDHandler.vhd
@@ -4,27 +4,26 @@ use ieee.std_logic_1164.all;
 entity LCDHandler is 
 	port (
 		clock: in std_logic;
-		--updateScreen: in std_logic;
-		--loginSuccess: in std_logic;
+		--resetPrint:	in std_logic;
 		system_state: in std_logic_vector(0 to 1);
-		busy       : OUT   STD_LOGIC := '1';  --lcd controller busy/idle feedback
+		current_number: in std_logic_vector(7 downto 0);
+		--busy       : OUT   STD_LOGIC := '1';  --lcd controller busy/idle feedback
 		rw, rs, e  : OUT   STD_LOGIC;  --read/write, setup/data, and enable for lcd
 		lcd_dataout   : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0));
 end LCDHandler;
 
 architecture handle of LCDHandler is
---signal lcd_busy: std_logic;
---signal lcd_data: std_logic_vector(0 to 7);
---signal lcd_en: std_logic;
---signal lcd_rw: std_logic;
---signal lcd_rs: std_logic;
 	signal con_en: std_logic;
 	signal con_bus: std_logic_vector(9 downto 0);
 	signal con_rs, con_rw: std_logic;
 	signal con_data: std_logic_vector(7 downto 0);
 	signal con_busy: std_logic;
-	--signal printing: std_logic;
 	signal updateScreen: std_logic;
+	signal enable_write: std_logic;
+	
+	signal busy: std_logic; --unused
+	
+	signal check: std_logic;
 	component lcd_controller is
 		PORT(
 		 clk        : IN    STD_LOGIC;  --system clock
@@ -35,19 +34,22 @@ architecture handle of LCDHandler is
 		 rw, rs, e  : OUT   STD_LOGIC;  --read/write, setup/data, and enable for lcd
 		 lcd_data   : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0));
 	end component;
+	
+	signal lastState: std_logic_vector(1 downto 0) := "00";
+	signal lastNum: std_logic_vector(7 downto 0);
 begin
 	--con_bus <= con_rs & con_rw & con_data;
 	lcdMaster: lcd_controller port map (clock, '1', con_en, con_bus, con_busy, rw, rs, e, lcd_dataout);
-	process(system_state(0))
-	begin
-		--cpos := 0;
-		updateScreen <= '1';
-	end process;
-	process(system_state(1))
-	begin
-		--cpos := 0;
-		updateScreen <= '1';
-	end process;
+--	process(system_state(0))
+--	begin
+--		--cpos := 0;
+--		updateScreen <= '1';
+--	end process;
+--	process(system_state(1))
+--	begin
+--		--cpos := 0;
+--		updateScreen <= '1';
+--	end process;
 	--process(updateScreen)
 	--begin
 		--if (updateScreen'event and updateScreen = '0') then
@@ -55,120 +57,183 @@ begin
 			--con_en <= '0';
 		--end if;
 	--end process;
-	process(clock)
-		variable cpos: integer range 0 to 16 := 0;
+	process(clock, system_state, current_number)
+		variable cpos: integer range 0 to 28 := 0;
 	begin
-		if (clock'event and clock = '1' and updateScreen = '1') then --and updateScreen'event
+		if (clock'event and clock = '1') then
 			if (con_busy = '0' and con_en = '0') then 
-				con_en <= '1';
-				if (cpos < 16) then 
-					cpos := cpos + 1;
-				else
+				if (not (system_state = lastState) or (not (current_number = lastNum))) then
 					cpos := 0;
+				end if;
+				
+				con_en <= '1';
+				if (cpos < 28) then 
+					cpos := cpos + 1;
 				end if;
 				if (system_state = "00") then --AWAITING INPUT
 					case cpos is
-						when 1 =>  con_bus <= "1001001001"; --I
-						when 2 =>  con_bus <= "1001101110"; --n
-						when 3 =>  con_bus <= "1001110011"; --s
-						when 4 =>  con_bus <= "1001101001"; --i
-						when 5 =>  con_bus <= "1001110010"; --r
-						when 6 =>  con_bus <= "1001100001"; --a
-						when 7 =>  con_bus <= "1000100000"; -- 
-						when 8 =>  con_bus <= "1001100001"; --a
-						when 9 =>  con_bus <= "1000100000"; -- 
-						when 10 => con_bus <= "1001110011"; --s
-						when 11 => con_bus <= "1001100101"; --e
-						when 12 => con_bus <= "1001101110"; --n
-						when 13 => con_bus <= "1001101000"; --h
-						when 14 => con_bus <= "1001100001"; --a
+						when 1 =>  con_bus <= "0000000010"; --reset
+						--when 1 =>  con_bus <= "1001001001"; --test
+						when 2 =>  con_bus <= "1001001001"; --I
+						when 3 =>  con_bus <= "1001101110"; --n
+						when 4 =>  con_bus <= "1001110011"; --s
+						when 5 =>  con_bus <= "1001101001"; --i
+						when 6 =>  con_bus <= "1001110010"; --r
+						when 7 =>  con_bus <= "1001100001"; --a
+						when 8 =>  con_bus <= "1000100000"; -- 
+						when 9 =>  con_bus <= "1001100001"; --a
+						when 10 => con_bus <= "1000100000"; -- 
+						when 11 => con_bus <= "1001110011"; --s
+						when 12 => con_bus <= "1001100101"; --e
+						when 13 => con_bus <= "1001101110"; --n
+						when 14 => con_bus <= "1001101000"; --h
+						when 15 => con_bus <= "1001100001"; --a
+						when 16 => con_bus <= "1000111010"; --:
+						when 17 => con_bus <= "0011000000"; -- newline
+						when 18 => con_bus <= "1000101000"; --( 
+						when 19 => con_bus <= "100011" & current_number(7 downto 4); -- 
+						when 20 => con_bus <= "100011" & current_number(3 downto 0); -- 
+						when 21 => con_bus <= "1000101001"; --)
+						when 22 => con_bus <= "1000100000"; -- 
+						when 23 => con_bus <= "1000100000"; -- 
+						when 24 => con_bus <= "1000100000"; -- 
+						when 25 => con_bus <= "1000100000"; -- 
+						when 26 => con_bus <= "1000100000"; -- 
+						when 27 => con_bus <= "1000100000"; -- 
+						
 						--when 15 => 
 							--updateScreen <= '0'; 
 							--cpos := 0;
 						--when 15 => cpos := 0;
 						when others => 
-							--con_en <= '0';
-							updateScreen <= '0'; 
+							con_en <= '0';
+							--updateScreen <= '0'; 
 							--cpos := 0;
 					end case;
 				end if;
-				if (system_state = "01") then --INCORRECT PASS
+				if (system_state = "01") then --NEW PASS
 					case cpos is
-						when 1 =>  con_bus <= "1001010011";	--S
-						when 2 =>  con_bus <= "1001100101"; --e
+						when 1 =>  con_bus <= "0000000010"; --reset
+						when 2 =>  con_bus <= "1001001001"; --I
 						when 3 =>  con_bus <= "1001101110"; --n
-						when 4 =>  con_bus <= "1001101000"; --h
-						when 5 =>  con_bus <= "1001100001"; --a
-						when 6 =>  con_bus <= "1000100000"; -- 
-						when 7 =>  con_bus <= "1001101001"; --i
-						when 8 =>  con_bus <= "1001101110"; --n
-						when 9 =>  con_bus <= "1001110110"; --v
-						when 10 => con_bus <= "1001100001"; --a
-						when 11 => con_bus <= "1001101100"; --l
-						when 12 => con_bus <= "1001101001"; --i
-						when 13 => con_bus <= "1001100100"; --d
+						when 4 =>  con_bus <= "1001110011"; --s
+						when 5 =>  con_bus <= "1001101001"; --i
+						when 6 =>  con_bus <= "1001110010"; --r
+						when 7 =>  con_bus <= "1001100001"; --a
+						when 8 =>  con_bus <= "1000100000"; -- 
+						when 9 =>  con_bus <= "1001100001"; --a
+						when 10 => con_bus <= "1000100000"; -- 
+						when 11 => con_bus <= "1001101110"; --n
+						when 12 => con_bus <= "1001101111"; --o
+						when 13 => con_bus <= "1001110110"; --v
 						when 14 => con_bus <= "1001100001"; --a
+						when 15 => con_bus <= "1000100000"; -- 
+						when 16 => con_bus <= "1000100000"; -- 
+						when 17 => con_bus <= "0011000000"; -- newline
+						when 18 => con_bus <= "1001110011"; --s
+						when 19 => con_bus <= "1001100101"; --e
+						when 20 => con_bus <= "1001101110"; --n
+						when 21 => con_bus <= "1001101000"; --h
+						when 22 => con_bus <= "1001100001"; --a
+						when 23 => con_bus <= "1000100000"; -- 
+						when 24 => con_bus <= "1000101000"; --( 
+						when 25 => con_bus <= "100011" & current_number(7 downto 4); -- 
+						when 26 => con_bus <= "100011" & current_number(3 downto 0); -- 
+						when 27 => con_bus <= "1000101001"; --)
 						--when 15 => 
 							--updateScreen <= '0'; 
 							--cpos := 0;
 						--when 15 => cpos := 0;
 						when others => 
-							--con_en <= '0';
-							updateScreen <= '0'; 
+							con_en <= '0';
+							--updateScreen <= '0'; 
 							--cpos := 0;
 					end case;
 				end if;
 				if (system_state = "10") then --LOGIN OK
 					case cpos is
-						when 1 =>  con_bus <= "1001000010"; --B
-						when 2 =>  con_bus <= "1001100101"; --e
-						when 3 =>  con_bus <= "1001101101"; --m
-						when 4 =>  con_bus <= "1000100000"; -- 
-						when 5 =>  con_bus <= "1001110110"; --v
-						when 6 =>  con_bus <= "1001101001"; --i
-						when 7 =>  con_bus <= "1001101110"; --n
-						when 8 =>  con_bus <= "1001100100"; --d
-						when 9 =>  con_bus <= "1001101111"; --o
-						when 10 => con_bus <= "1000101000"; --( 
-						when 11 => con_bus <= "1001100001"; --a
-						when 12 => con_bus <= "1000101001"; --)
-						when 13 => con_bus <= "1000100001"; --!
+						when 1 =>  con_bus <= "0000000010"; --reset
+						--when 1 =>  con_bus <= "1001000010"; --test
+						when 2 =>  con_bus <= "1001000010"; --B
+						when 3 =>  con_bus <= "1001100101"; --e
+						when 4 =>  con_bus <= "1001101101"; --m
+						when 5 =>  con_bus <= "1000100000"; -- 
+						when 6 =>  con_bus <= "1001110110"; --v
+						when 7 =>  con_bus <= "1001101001"; --i
+						when 8 =>  con_bus <= "1001101110"; --n
+						when 9 =>  con_bus <= "1001100100"; --d
+						when 10 => con_bus <= "1001101111"; --o
+						when 11 => con_bus <= "1000101000"; --( 
+						when 12 => con_bus <= "1001100001"; --a
+						when 13 => con_bus <= "1000101001"; --)
+						when 14 => con_bus <= "1000100001"; --!
+						when 15 => con_bus <= "1000100000"; -- 
+						when 16 => con_bus <= "1000100000"; -- 
+						when 17 => con_bus <= "0011000000"; -- newline
+						when 18 => con_bus <= "1000100000"; -- 
+						when 19 => con_bus <= "1000100000"; -- 
+						when 20 => con_bus <= "1000100000"; -- 
+						when 21 => con_bus <= "1000100000"; -- 
+						when 22 => con_bus <= "1000100000"; -- 
+						when 23 => con_bus <= "1000100000"; -- 
+						when 24 => con_bus <= "1000100000"; -- 
+						when 25 => con_bus <= "1000100000"; -- 
+						when 26 => con_bus <= "1000100000"; -- 
+						when 27 => con_bus <= "1000100000"; --
 						--when 14 => 
 							--updateScreen <= '0'; 
 							--cpos := 0;
 						--when 14 => cpos := 0;
 						when others => 
-							--con_en <= '0';
-							updateScreen <= '0'; 
+							con_en <= '0';
+							--updateScreen <= '0'; 
 							--cpos := 0;
 					end case;
 				end if;
 				if (system_state = "11") then --LOCKDOWN
 					case cpos is 
-						when 1 =>  con_bus <= "1001010100"; --T
-						when 2 =>  con_bus <= "1001110010"; --r
-						when 3 =>  con_bus <= "1001100001"; --a
-						when 4 =>  con_bus <= "1001110110"; --v
-						when 5 =>  con_bus <= "1001100001"; --a
-						when 6 =>  con_bus <= "1001100100"; --d
-						when 7 =>  con_bus <= "1001101111"; --o
-						when 8 =>  con_bus <= "1000100001"; --!
+						when 1 =>  con_bus <= "0000000010"; --reset
+						when 2 =>  con_bus <= "1001010100"; --T
+						when 3 =>  con_bus <= "1001110010"; --r
+						when 4 =>  con_bus <= "1001100001"; --a
+						when 5 =>  con_bus <= "1001110110"; --v
+						when 6 =>  con_bus <= "1001100001"; --a
+						when 7 =>  con_bus <= "1001100100"; --d
+						when 8 =>  con_bus <= "1001101111"; --o
+						when 9 =>  con_bus <= "1000100001"; --!
+						when 10 => con_bus <= "1000100000"; -- 
+						when 11 => con_bus <= "1000100000"; -- 
+						when 12 => con_bus <= "1000100000"; -- 
+						when 13 => con_bus <= "1000100000"; -- 
+						when 14 => con_bus <= "1000100000"; -- 
+						when 15 => con_bus <= "1000100000"; -- 
+						when 16 => con_bus <= "1000100000"; -- 
+						when 17 => con_bus <= "0011000000"; -- newline
+						when 18 => con_bus <= "1000100000"; -- 
+						when 19 => con_bus <= "1000100000"; -- 
+						when 20 => con_bus <= "1000100000"; -- 
+						when 21 => con_bus <= "1000100000"; -- 
+						when 22 => con_bus <= "1000100000"; -- 
+						when 23 => con_bus <= "1000100000"; -- 
+						when 24 => con_bus <= "1000100000"; -- 
+						when 25 => con_bus <= "1000100000"; -- 
+						when 26 => con_bus <= "1000100000"; -- 
+						when 27 => con_bus <= "1000100000"; --
 						--when 9 =>  
 							--updateScreen <= '0'; 
 							--cpos := 0;
 						--when 9 => cpos := 0;
 						when others => 
-							--con_en <= '0';-- cpos := 0;
-							updateScreen <= '0'; 
+							con_en <= '0';-- cpos := 0;
+							--updateScreen <= '0'; 
 							--cpos := 0;
 					end case;
 				end if;
 			else 
 				con_en <= '0';
 			end if;
-		--else 
-			--cpos := 0;
-			--con_en <= '0';
+		lastState <= system_state;
+		lastNum <= current_number;
 		end if;
 	end process;
 end handle;
--- projeto.vhd
+++ projeto.vhd
@@ -4,109 +4,215 @@ use ieee.std_logic_unsigned.all;
 
 entity projeto is 
 	port (entradaSENHA: 	in std_logic_vector(0 to 5);
+	
 			chaveE:			in std_logic;
 			chaveS: 		in std_logic;
+			
 			resetStrikes:	in std_logic;
+			
 			masterClock: in std_logic;
+			
 			ledEstado: 		out std_logic;
 			ledErro:		out std_logic;
+			
 			numero0:		out std_logic_vector(0 to 6);
 			numero1: 	out std_logic_vector(0 to 6);
+			
+			numeroA:		out std_logic_vector(0 to 6);
 			lcd_rw, lcd_rs, lcd_e  : OUT   STD_LOGIC;  --read/write, setup/data, and enable for lcd
 			lcd_dataout   : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0);
+			
 			senhaOKLED: out std_logic);
 end projeto;
 
 architecture cofre of projeto is
-	signal EnterTrigger, PassOK, IsLoggedIn, SystemLockdown, OnKeyUpdate, SetLogin, ResetLogin, NewKey, WrongPass, NewETrig, NewAttempt, ajuste, ResetLockdown: std_logic;
-	signal senhaAtual: std_logic_vector(0 to 5);
-	signal ultimaTent: std_logic_vector(0 to 5);
-	signal bcd0, bcd1: std_logic_vector(0 to 3);
-	signal attempts: std_logic_vector(0 to 3);
-	signal LCD_BUSY: std_logic;
-	signal LCD_UPDT: std_logic;
-	signal SYS_STATE: std_logic_vector(0 to 1);
+	TYPE CONTROL IS (waiting_input, test_input, login_success, read_new_key, out_of_attempts);
+	SIGNAL STATE: CONTROL;
+
+	TYPE BCDCTRL IS (show_input, clear_bcd, show_error);
+	SIGNAL BCDSTATE: BCDCTRL := clear_bcd;
+	
 	component bcd
 		port (code: in  std_logic_vector(0 to 3);
 				leds: out std_logic_vector(0 to 6));
 	end component;
-	component flipflopd
-		port (d, clock, en, r: in std_logic;
-				q 		  : out std_logic);
-	end component;
+	
 	component compare
 		port (inp1, inp2: in std_logic_vector(0 to 5);
 				res: out std_logic);
 	end component;
-	component memoryLine
-		port (Data: in std_logic_vector(0 to 5);
-				Clock, Enable, Reset: in std_logic;
-				Qout: out std_logic_vector(0 to 5));
-	end component;
-	component upcounter is 
+	
+	component LCDHandler
 		port (
 			clock: in std_logic;
-			enable: in std_logic;
-			reset: in std_logic;
-			output: out std_logic_vector(0 to 1));
-	end component;
-	component LCDHandler is
-		port (
-			clock: in std_logic;
-			--updateScreen: in std_logic;
 			system_state: in std_logic_vector(0 to 1);
-			busy       : OUT   STD_LOGIC := '1';  --lcd controller busy/idle feedback
+			current_number: in std_logic_vector(7 downto 0);
+			--busy       : OUT   STD_LOGIC := '1';  --lcd controller busy/idle feedback
 			rw, rs, e  : OUT   STD_LOGIC;  --read/write, setup/data, and enable for lcd
 			lcd_dataout   : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0));
 	end component;
+
+	signal PassEQ:	std_logic := '1';
+	signal PassOK:	std_logic := '0';
+	signal lastE:	std_logic := '1';
+	signal lastK:	std_logic_vector(0 to 5) := "000000";
+	signal storeK:	std_logic_vector(0 to 5) := "000000";
+
+	--signal showKeyState:	std_logic_vector(0 to 1);
+	signal bcd0, bcd1:	std_logic_vector(0 to 3);
+	signal bcd0t, bcd1t:	std_logic_vector(0 to 3);
+	signal AUX0, AUX1, AUX2, aux3, AUXILIAR, RESULT: std_logic_vector(0 to 7);
+
+	signal storeA: std_logic_vector(0 to 1) := "00";
+	
+	signal Sys_ST:	std_logic_vector(0 to 1) := "00";
 begin
-	process(IsLoggedIn, SystemLockdown, EnterTrigger)
-	begin
-		if (SystemLockdown = '0') then 
-			SYS_STATE <= "11";
-		elsif (IsLoggedIn = '1') then 
-			SYS_STATE <= "10";
-		elsif (EnterTrigger = '1') then 
-			SYS_STATE <= "01";
-		else
-			SYS_STATE <= "00";
-		end if;
-	end process;
-	masterHandler: LCDHandler port map (masterClock, SYS_STATE, LCD_BUSY, lcd_rw, lcd_rs, lcd_e, lcd_dataout);
-	--LCD_UPDT <= IsLoggedIn;
-	LCD_UPDT <= '1';
+	compareLast:	compare port map(entradaSENHA, lastK, PassEQ);
+	compareKey:		compare port map(entradaSENHA, storeK, PassOK);
+	
 	senhaOKLED <= PassOK;
-	checkPass: compare port map (entradaSENHA, senhaAtual, PassOK);
-	
-	NewKey <= not chaveS;
-	EnterTrigger <= chaveE and SystemLockdown;
-	loggedState: flipflopd port map (SetLogin, OnKeyUpdate, '1', ResetLogin, IsLoggedIn);
-	SetLogin <= IsLoggedIn or PassOK;
-	ResetLogin <= not EnterTrigger;
-	
-	guardaSenha: memoryLine port map (entradaSENHA, NewKey, IsLoggedIn, '0', senhaAtual);
-	tentaE: flipflopd port map (EnterTrigger, OnKeyUpdate, '1', IsLoggedIn, NewETrig);
-	
-	guardaUltimaTent: memoryLine port map (entradaSENHA, OnKeyUpdate, '1', '0', ultimaTent);
-	checkForNewAttempt: compare port map (entradaSENHA, ultimaTent, NewAttempt);
-	
-	process(EnterTrigger)
-	begin 
-		OnKeyUpdate <= EnterTrigger and (NewETrig nand NewAttempt);
-	end process;
-	
-	WrongPass <= not PassOK;
-	--countLockdown: upcounter port map (OnKeyUpdate, not IsLoggedIn, ResetLockdown, attempts);
-	strike1: flipflopd port map (  WrongPass, EnterTrigger, not IsLoggedIn, ResetLockdown, attempts(0));
-	strike2: flipflopd port map (attempts(0), EnterTrigger, not IsLoggedIn, ResetLockdown, attempts(1));
-	strike3: flipflopd port map (attempts(1), EnterTrigger, not IsLoggedIn, ResetLockdown, attempts(2));
-	
-	SystemLockdown <= not (attempts(0) and attempts(1) and attempts(2));
-	ResetLockdown <= IsLoggedIn or not resetStrikes;
+	PROCESS(masterClock)
+		VARIABLE attempts : INTEGER := 0;
+	BEGIN
+		IF(masterClock'EVENT and masterClock = '1') THEN
+			CASE state IS 
+				WHEN waiting_input =>
+					Sys_ST <= "00";
+					ledEstado <= '0';
+					IF (chaveE = '1' and lastE = '0') THEN 
+						state <= test_input;
+						lastE <= '1';
+					ELSIF (chaveE = '1' and PassEQ = '0') THEN 
+						state <= test_input;
+					END IF;
+					IF (chaveE = '0') THEN 
+						lastE <= '0';
+					END IF;
+					
+				WHEN test_input =>
+					IF (PassOK = '1') THEN 
+						state <= login_success;
+					ELSE
+						BCDSTATE <= show_error;
+						lastK <= entradaSENHA;
+						attempts := attempts + 1;
+						IF (attempts < 3) THEN 
+							state <= waiting_input;
+						ELSE 
+							state <= out_of_attempts;
+						END IF;
+					END IF;
+					
+				WHEN login_success =>
+					BCDSTATE <= show_input;
+					Sys_ST <= "10";
+					ledEstado <= '1';
+					attempts := 0;
+					IF (chaveE = '0') THEN 
+						BCDSTATE <= clear_bcd;
+						ledEstado <= '0';
+						state <= waiting_input;
+						
+					ELSIF (chaveS = '1') THEN 
+						state <= read_new_key;
+					END IF;
+					
+				WHEN read_new_key =>
+					Sys_ST <= "01";
+					IF (chaveE = '0') THEN 
+						BCDSTATE <= clear_bcd;
+						state <= waiting_input;
+						
+					ELSIF (chaveS = '0') THEN
+						storeK <= entradaSENHA;
+						state <= login_success;
+					END IF;
+					
+				WHEN out_of_attempts =>
+					Sys_ST <= "11";
+					ledErro <= '1';
+					IF (resetStrikes = '0') THEN --Pushbutton invertido
+						ledErro <= '0';
+						attempts := 0;
+						state <= waiting_input;
+					END IF;
+			END CASE;
+		END IF;
 		
+		case (attempts) is
+			when 0 => storeA <= "00";
+			when 1 => storeA <= "01";
+			when 2 => storeA <= "10";
+			when others => storeA <= "11";
+		end case;
+		
+		if (resetStrikes = '0') then
+			state <= waiting_input;
+			attempts := 0;
+			ledErro <= '0';
+			ledEstado <= '0';
+			BCDSTATE <= clear_bcd;
+		end if;
+		
+		AUX0 <= ("0000" & ENTRADASENHA(2 TO 5));
+				
+		IF (AUX0 > 9) THEN
+			AUX2 <= AUX0 + 6;
+		ELSE 
+			AUX2 <= AUX0;
+		END IF;
+		--AUX2 <= AUX0 + 6 WHEN (AUX0 > 9) ELSE AUX0;
+		
+		IF (ENTRADASENHA(0) = '1') THEN
+			AUX1 <= AUX2 + "00110010";
+		ELSE
+			AUX1 <= AUX2;
+		END IF;
+		--AUX1 <= AUX2 + "00110010" WHEN (ENTRADASENHA(0) = '1') ELSE AUX2;
+		
+		IF (AUX1(4 TO 7) > 9) THEN
+			AUX3 <= AUX1 + 6;
+		ELSE
+			AUX3 <= AUX1;
+		END IF;
+		--aux3 <= aux1 + 6 WHEN (AUX1(4 to 7) > 9) ELSE AUX1;
+		
+		IF (ENTRADASENHA(1) = '1') THEN
+			AUXILIAR <= AUX3 + "00010110";
+		ELSE
+			AUXILIAR <= AUX3;
+		END IF;
+		--AUXILIAR <= AUX3 + "00010110" WHEN (ENTRADASENHA(1) = '1') ELSE AUX3;
+		
+		IF (AUXILIAR(4 TO 7) > 9) THEN
+			RESULT <= AUXILIAR + 6;
+		ELSE
+			RESULT <= AUXILIAR;
+		END IF;
+		--RESULT <= AUXILIAR + 6 WHEN AUXILIAR(4 TO 7) > 9 ELSE AUXILIAR;
+		
+		BCD0T <= RESULT(4 TO 7);
+		BCD1T <= RESULT(0 TO 3);
+				
+		case BCDSTATE is
+			when show_input => 
+				BCD0 <= BCD0T;
+				BCD1 <= BCD1T;
+				
+			when clear_bcd =>		--Clear display
+				BCD0 <= "1010";
+				BCD1 <= "1010";
+				
+			when show_error =>	--Display "--"
+				BCD0 <= "1011";
+				BCD1 <= "1011";
+		end case;
+	END PROCESS;
+	
 	num0: bcd port map (bcd0, numero0);
 	num1: bcd port map (bcd1, numero1);
 	
-	ledEstado <= IsLoggedIn;
-	ledErro <= not SystemLockdown;
+	numA: bcd port map ("00" & storeA, numeroA);
+	
+	lcdhan: LCDHandler port map(masterClock, Sys_ST, bcd1t & bcd0t, lcd_rw, lcd_rs, lcd_e, lcd_dataout);
 end cofre;
